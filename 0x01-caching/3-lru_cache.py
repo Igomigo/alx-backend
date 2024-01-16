2afc11798915b543df14fe5a10f86cb463679a7a
Igomigo <igomigofatai@gmail.com>
2024-01-16 20:37:09 +0000

    LRU algorithm implemented

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -16,16 +16,15 @@ class LRUCache(BaseCaching):
         """ discards values from the cache using the LRU strategy """
         if key is not None and item is not None:
             self.cache_data[key] = item
-            
             if key not in self.freq_used:
                 self.freq_used.append(key)
             else:
                 self.freq_used.append(
                     self.freq_used.pop(self.freq_used.index(key)))
             if len(self.freq_used) > BaseCaching.MAX_ITEMS:
-                    least_used = self.freq_used.pop(0)
-                    del self.cache_data[least_used]
-                    print(f"DISCARD: {least_used}")
+                least_used = self.freq_used.pop(0)
+                del self.cache_data[least_used]
+                print(f"DISCARD: {least_used}")
 
     def get(self, key):
         """  return the value in self.cache_data linked to key """
